7dd1ffee7894f32fe148b1b13e9e314aef0118da
Esteban Palomeque <shaggy.palomeque@gmail.com>
2023-02-07 00:03:56 -0500

    Add cards to architecture definition organism

--- src/components/organisms/ArchitectureDefinition.tsx
+++ src/components/organisms/ArchitectureDefinition.tsx
@@ -1,7 +1,7 @@
 import { ListItem, Title } from '@/components/atoms'
 import { Card, List } from '@/components/molecules'
 
-import { atom, page } from '@/data'
+import { atom, molecule, page } from '@/data'
 import Image from 'next/image'
 
 export const ArchitectureDefinition = () => {
@@ -34,6 +34,42 @@ export const ArchitectureDefinition = () => {
                     />
                 </div>
             </Card>
+
+            <div className="block sm:flex justify-between ">
+                <Card className="block w-full sm:flex justify-between items-center py-16 px-12 !from-lime-600 !to-cyan-500 sm:w-1/2 mr-5 mb-5">
+                    <div className="w-full">
+                        <h2 className="text-3xl sm:text-4xl font-bold">
+                            Molécula
+                        </h2>
+                        <p className="my-5 text-md sm:text-2xl">
+                            Representa la agrupación de átomos
+                        </p>
+
+                        <List>
+                            {molecule.map((item) => (
+                                <ListItem key={item}>{item}</ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Card>
+
+                <Card className="block w-full sm:flex justify-between items-center py-16 px-12 !from-sky-500 !to-fuchsia-400 sm:w-1/2 mb-5">
+                    <div className="w-full">
+                        <h2 className="text-3xl sm:text-4xl font-bold">
+                            Molécula
+                        </h2>
+                        <p className="my-5 text-md sm:text-2xl">
+                            Representa la agrupación de átomos
+                        </p>
+
+                        <List>
+                            {molecule.map((item) => (
+                                <ListItem key={item}>{item}</ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Card>
+            </div>
 
             <Card className="block sm:flex justify-between items-center py-16 px-12 from-violet-500 !to-cyan-400">
                 <div className="w-full sm:w-1/2">
