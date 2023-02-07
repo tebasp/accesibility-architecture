32dc5b0071ef731316beb29c8dfbca54e8663cd5
Esteban Palomeque <shaggy.palomeque@gmail.com>
2023-02-06 19:04:10 -0500

    Add aria label to generic button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -6,6 +6,7 @@ interface Props {
     url?: string
     className?: string
     onClick?: () => void
+    ariaLabel?: string
 }
 
 export const Button: FC<Props> = ({
@@ -13,6 +14,7 @@ export const Button: FC<Props> = ({
     className = '',
     children,
     onClick,
+    ariaLabel = '',
 }) => {
     return (
         <Link
@@ -20,6 +22,7 @@ export const Button: FC<Props> = ({
             onClick={onClick}
             className={`fade-in relative text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 transition-all hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-slate-400 dark:focus:ring-slate-400 font-light rounded-lg text-sm px-5 py-2.5 text-center
             ${className}`}
+            aria-label={ariaLabel}
         >
             {children}
         </Link>
