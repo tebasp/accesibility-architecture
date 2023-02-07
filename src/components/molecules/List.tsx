89b25fa709b7145dd2dd25885ca7fcf571cfc7a1
Esteban Palomeque <shaggy.palomeque@gmail.com>
2023-02-06 23:47:22 -0500

    Add classname to list

--- src/components/molecules/List.tsx
+++ src/components/molecules/List.tsx
@@ -1,7 +1,14 @@
+import { FC } from 'react'
+
 interface Props {
     children: JSX.Element | JSX.Element[]
+    className?: string
 }
 
-export const List = ({ children }: Props) => {
-    return <ul>{children}</ul>
+export const List: FC<Props> = ({ children, className }) => {
+    return (
+        <ul className={`list-disc list-inside text-sm sm:text-lg ${className}`}>
+            {children}
+        </ul>
+    )
 }
